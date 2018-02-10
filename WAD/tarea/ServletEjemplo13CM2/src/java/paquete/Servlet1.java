588bf5e9ed9b791a941ed072cda50101de649218
Jorge Reus <j-g1996@live.com>
2018-02-10 17:43:04 -0600

    Added anotatios for servlet1

--- WAD/tarea/ServletEjemplo13CM2/src/java/paquete/Servlet1.java
+++ WAD/tarea/ServletEjemplo13CM2/src/java/paquete/Servlet1.java
@@ -3,6 +3,7 @@ package paquete;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author reus
  */
+@WebServlet(
+        name = "Servlet1",
+        description = "Primer Servlet de Ejemplo",
+        urlPatterns = {"/Servlet1"}
+)
 public class Servlet1 extends HttpServlet {
 
     @Override
