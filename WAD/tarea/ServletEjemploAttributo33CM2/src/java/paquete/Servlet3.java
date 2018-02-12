c2aaafb5c2b5711782faed765cd04ef8cf558f05
Jorge Reus <j-g1996@live.com>
2018-02-12 08:28:25 -0600

    Added Servlet 2, 3 and 3_1

--- WAD/tarea/ServletEjemploAttributo33CM2/src/java/paquete/Servlet3.java
+++ WAD/tarea/ServletEjemploAttributo33CM2/src/java/paquete/Servlet3.java
@@ -1,27 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paquete;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author reus
  */
-@WebServlet(
-        name = "Servlet1",
-        description = "Primer Servlet de Ejemplo",
-        urlPatterns = {"/Servlet1"}
-)
-public class Servlet1 extends HttpServlet {
+public class Servlet3 extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String param1 = request.getParameter("nombre");
+        HttpSession session = request.getSession();
+        session.setAttribute("attribNombre", param1);
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<html>");
@@ -29,7 +33,7 @@ public class Servlet1 extends HttpServlet {
         out.println("<title>Servlet Servlet1</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Hello World " + request.getContextPath() + "</h1>");
+        out.println("<h1>Hello World " + param1 + "</h1>");
         out.println("</body>");
         out.println("</html>");
     }
--- WAD/tarea/ServletEjemploAttributo33CM2/src/java/paquete/Servlet3_1.java
+++ WAD/tarea/ServletEjemploAttributo33CM2/src/java/paquete/Servlet3_1.java
@@ -1,27 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paquete;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author reus
  */
-@WebServlet(
-        name = "Servlet1",
-        description = "Primer Servlet de Ejemplo",
-        urlPatterns = {"/Servlet1"}
-)
-public class Servlet1 extends HttpServlet {
+public class Servlet3_1 extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        String nombre = session.getAttribute("attribNombre").toString();
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<html>");
@@ -29,7 +32,7 @@ public class Servlet1 extends HttpServlet {
         out.println("<title>Servlet Servlet1</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Hello World " + request.getContextPath() + "</h1>");
+        out.println("<h1>Hello World, attribute:" + nombre + "</h1>");
         out.println("</body>");
         out.println("</html>");
     }
