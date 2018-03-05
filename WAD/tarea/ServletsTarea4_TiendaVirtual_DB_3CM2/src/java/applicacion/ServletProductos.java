c1f30efcfae9cf5d9b911b9d41a6a26f72a86554
Jorge Reus <j-g1996@live.com>
2018-03-05 08:55:53 -0600

    Up to date

--- WAD/tarea/ServletsTarea4_TiendaVirtual_DB_3CM2/src/java/applicacion/ServletProductos.java
+++ WAD/tarea/ServletsTarea4_TiendaVirtual_DB_3CM2/src/java/applicacion/ServletProductos.java
@@ -62,49 +62,52 @@ public class ServletProductos extends HttpServlet {
         out.println("<script src=\"./js/functions.js\" ></script>");
         out.println("</head>");
         out.println("<body>");
+        out.println("<form id='prodForm' method='GET' action='ServletProductos'>");
         out.println("<h1>Departamento de Electrónicos</h1>");
-        String html = "<form id=\"prodForm\" method=\"GET\" action=\"ServletProductos\">\n"
-                + "    <input id=\"cbXboxOne\" type=\"checkbox\" name=\"productos\" value=\"XboxOne\" "
-                + (productos.contains("XboxOne") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbXboxOne\">Xbox One</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbPs4\" type=\"checkbox\" name=\"productos\" value=\"Ps4\" "
-                + (productos.contains("Ps4") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbPs4\">Playstation 4</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbSwitch\" type=\"checkbox\" name=\"productos\" value=\"Switch\" "
-                + (productos.contains("Switch") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbSwitch\">Nintendo Switch</label><br>\n"
-                + "\n"
-                + "<h1>Departamento de Ropa</h1>"
-                + "    <input id=\"cbPlayera\" type=\"checkbox\" name=\"productos\" value=\"Playera\" "
-                + (productos.contains("Playera") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbPlayera\">Playera</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbChamarra\" type=\"checkbox\" name=\"productos\" value=\"Chamarra\" "
-                + (productos.contains("Chamarra") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbChamarra\">Chamarra</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbZapatos\" type=\"checkbox\" name=\"productos\" value=\"Zapatos\" "
-                + (productos.contains("Zapatos") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbZapatos\">Zapatos</label><br>\n"
-                + "\n"
-                + "<h1>Departamento de Electrodomésticos</h1>"
-                + "    <input id=\"cbLavadora\" type=\"checkbox\" name=\"productos\" value=\"Lavadora\" "
-                + (productos.contains("Lavadora") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbLavadora\">Lavadora</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbLicuadora\" type=\"checkbox\" name=\"productos\" value=\"Licuadora\" "
-                + (productos.contains("Licuadora") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbLicuadora\">Licuadora</label><br>\n"
-                + "\n"
-                + "    <input id=\"cbRefrigerador\" type=\"checkbox\" name=\"productos\" value=\"Refrigerador\" "
-                + (productos.contains("Refrigerador") ? "checked>\n" : ">\n")
-                + "    <label for=\"cbRefrigerador\">Refrigerador</label><br>\n"
-                + "\n"
-                + "    <button id=\"checkout\">Checkout</button>\n"
-                + "</form>";
-        out.println(html);
+        try {
+            Statement s;
+            ResultSet rs;
+            Connection con = null;
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/cart", "root", "root");
+            s = con.createStatement();
+            rs = s.executeQuery("SELECT nombre FROM cart WHERE departamento='Electronicos'");
+            while (rs.next()) {
+                String inp = "<input type='checkbox' id='" +rs.getString("cart.nombre") 
+                        + "' name='productos' value='" + rs.getString("cart.nombre") +  "'"
+                        + (productos.contains(rs.getString("cart.nombre")) ? "checked>\n" : ">\n")
+                        + "<label for='" + rs.getString("cart.nombre") + "'>" 
+                        + rs.getString("cart.nombre") +  "</label><br>";
+                out.println(inp);
+            }
+            out.println("<h1>Departamento de Ropa</h1>");
+            rs = s.executeQuery("SELECT nombre FROM cart WHERE departamento='Ropa'");
+            while (rs.next()) {
+                String inp = "<input type='checkbox' id='" +rs.getString("cart.nombre") 
+                        + "' name='productos' value='" + rs.getString("cart.nombre") +  "'"
+                        + (productos.contains(rs.getString("cart.nombre")) ? "checked>\n" : ">\n")
+                        + "<label for='" + rs.getString("cart.nombre") + "'>" 
+                        + rs.getString("cart.nombre") +  "</label><br>";
+                out.println(inp);
+            }
+            out.println("<h1>Departamento de Electrodomésticos</h1>");
+            rs = s.executeQuery("SELECT nombre FROM cart WHERE departamento='Electrodomesticos'");
+            while (rs.next()) {
+                String inp = "<input type='checkbox' id='" +rs.getString("cart.nombre") 
+                        + "' name='productos' value='" + rs.getString("cart.nombre") +  "'"
+                        + (productos.contains(rs.getString("cart.nombre")) ? "checked>\n" : ">\n")
+                        + "<label for='" + rs.getString("cart.nombre") + "'>" 
+                        + rs.getString("cart.nombre") +  "</label><br>";
+                out.println(inp);
+            }
+            out.println("<button id='checkout'>Checkout</button></form>");
+            con.close();
+        } catch (SQLException e) {
+            System.err.println("Error sql al intentar conectarse con la base de datos");
+            e.printStackTrace();
+        } catch (ClassNotFoundException ee) {
+            System.err.println("No se puede encontrar la clase:" + ee);
+        }
         String modal = "<button id=\"myBtn\" type=\"button\">Ver Carrito</button>\n"
                 + "\n"
                 + "            <!-- The Modal -->\n"
