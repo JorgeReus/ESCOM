ff7edf92e7a534a5ccd1258ec911e1b0746f52b9
Jorge Reus <j-g1996@live.com>
2018-03-12 10:33:43 -0600

    Up to date

---
+++ WAD/tarea/Ejemplo25_loginFilter_3CM2/src/java/admin/ServletLogin.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author reus
+ */
+public class ServletLogin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String id = (String) request.getParameter("id");
+        String pass = (String) request.getParameter("pass");
+        LoginBean lb = new LoginBean();
+        if (lb.validateUser(id, pass)){           
+            HttpSession session = request.getSession(true);
+            session.setAttribute("userName", id);
+            response.sendRedirect("ServletWelcome");
+        } else {
+            response.sendRedirect("ServletFailure");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
---
+++ WAD/tarea/Tarea05_TiendaVirtualLoginEncodeURL_3CM2_1/src/java/applicacion/ServletInicio.java
@@ -0,0 +1,47 @@
+package applicacion;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author reus
+ */
+public class ServletInicio extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Index</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<a href='ServletElectronicos'>Eletronicos</a>");
+            out.println("<a href='ServletRopa'>Ropa</a>");
+            out.println("<a href='ServletElectronicos'>Eletronicos</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
