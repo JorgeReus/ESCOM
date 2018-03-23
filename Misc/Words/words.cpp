367afbb0012a263ea5258262678c13df4bc05f14
Jorge Reus <j-g1996@live.com>
2018-03-23 01:24:25 -0600

    Up to date

--- Misc/Words/words.cpp
+++ Misc/Words/words.cpp
@@ -1,43 +1,76 @@
 #include <iostream>
 #include <map>
+#include <stdio.h>
 #include <string>
 #include <iterator>
 #include <sys/types.h> 
 #include <dirent.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <fcntl.h>
+#include <string>
+#include "file.h"
+#include <vector>
+#include <sstream>
+#include <locale>
+#include <set>
 
 
+using namespace std;
 int main(int argc, char *argv[])
 {   
-    if (argc != 2) {
+    locale loc("es_MX.utf8");
+    if (argc != 2)
         exit(1);
-    }
+    queue<string> files;
     DIR *directory = opendir("./files");
     struct dirent *dir;
-    while ((dir = readdir(directory)) != NULL) {
-        std::cout <<  dir->d_name << std::endl;
-    }
-    //std::map<std::string, int> mapOfWords;
-    // mapOfWords["sun"] = 3;
-    // Will replace the value of already added key i.e. earth
-    // mapOfWords["earth"] = 4;
-    // Iterate through all elements in std::map
-    // std::map<std::string, int>::iterator it = mapOfWords.begin();
-    /*while(it != mapOfWords.end())
+    while ((dir = readdir(directory)) != NULL)
+        if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, ".."))
+            files.push(dir->d_name);
+    // Declaring the type of Predicate that accepts 2 pairs and return a bool
+    typedef std::function<bool(std::pair<std::string, int>, std::pair<std::string, int>)> Comparator;
+    // Defining a lambda function to compare two pairs. It will compare two pairs using second field
+    Comparator compFunctor = [](std::pair<std::string, int> elem1 ,std::pair<std::string, int> elem2)
     {
-        std::cout<<it->first<<" :: "<<it->second<<std::endl;
-        it++;
+        return elem1.second > elem2.second;
+    };
+    string filename = files.front();
+    Archivo input("./files/" + filename);
+    int nbytes;
+    while((nbytes = input.lee(BUFSIZ)) > 0);
+    istringstream iss(input.get_contenido());
+    vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
+    map<string, int> mapOfWords;
+    for(auto const& token: tokens) {
+        string s = token;
+        // Remove non alpha characters
+        for(int i = 0; i <= s.size(); ++i)
+        {
+            //if(!isalpha(s[i]))
+            if ((s[i] > ' ' && s[i] <= '@') || s[i]== *u8"¿")
+                s[i] = ' ';
+        }
+        std::string::iterator end_pos = remove(s.begin(), s.end(), ' ');
+        s.erase(end_pos, s.end());
+        // Not Found
+        if(mapOfWords.find(s) != mapOfWords.end()){          
+            mapOfWords[s]++;
+        }
+        else 
+            mapOfWords[s] = 1;
     }
-    // Check if insertion is successful or not
-    if(mapOfWords.insert(std::make_pair("earth", 1)).second == false)
-    {
-        std::cout<<"Element with key 'earth' not inserted because already existed"<<std::endl;
+    set<std::pair<string, int>, Comparator> setOfWords(
+            mapOfWords.begin(), mapOfWords.end(), compFunctor);
+    int i=0; 
+    for (std::pair<std::string, int> element : setOfWords){
+        if (i <= 40){
+            std::cout << element.first << " :: " << element.second << std::endl;
+            i++;
+        }
     }
-    // Searching element in std::map by key.
-    if(mapOfWords.find("sun") != mapOfWords.end())
-        std::cout<<"word 'sun' found"<<std::endl;
-    if(mapOfWords.find("mars") == mapOfWords.end())
-    std::cout<<"word 'mars' not found"<<std::endl;*/    
     return 0;
 }
