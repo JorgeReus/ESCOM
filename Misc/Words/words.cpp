cdd741087c6068747211b87db08d171b168810fd
Jorge Reus <j-g1996@live.com>
2018-03-23 21:25:06 -0600

    Up to date

--- Misc/Words/words.cpp
+++ Misc/Words/words.cpp
@@ -44,6 +44,10 @@ public:
     {
         return m_queque.front();
     }
+    bool empty()
+    {
+        return  m_queque.empty();
+    }
 
 private:
     std::queue<T> m_queque;
@@ -52,50 +56,54 @@ private:
 
 my_queue<string> files;
 
+// Declaring the type of Predicate that accepts 2 pairs and return a bool
+typedef std::function<bool(std::pair<std::string, int>, std::pair<std::string, int>)> Comparator;
+// Defining a lambda function to compare two pairs. It will compare two pairs using second field
+Comparator compFunctor = [](std::pair<std::string, int> elem1 ,std::pair<std::string, int> elem2)
+{
+    return elem1.second > elem2.second;
+};
+
 void calculateWords()
 {
-    string filename = files.front();
-    files.pop();
-    // Declaring the type of Predicate that accepts 2 pairs and return a bool
-    typedef std::function<bool(std::pair<std::string, int>, std::pair<std::string, int>)> Comparator;
-    // Defining a lambda function to compare two pairs. It will compare two pairs using second field
-    Comparator compFunctor = [](std::pair<std::string, int> elem1 ,std::pair<std::string, int> elem2)
+    while(!files.empty()) 
     {
-        return elem1.second > elem2.second;
-    };
-    Archivo input("./files/" + filename);
-    int nbytes;
-    while((nbytes = input.lee(BUFSIZ)) > 0);
-    istringstream iss(input.get_contenido());
-    vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
-    map<string, int> mapOfWords;
-    for(auto const& token: tokens) {
-        string s = token;
-        // Remove non alpha characters
-        for(int i = 0; i <= s.size(); ++i)
-        {
-            if ((s[i] > ' ' && s[i] <= '@') || s[i]== *u8"¿")
-                s[i] = ' ';
+        string filename = files.front();
+        files.pop();
+        Archivo input("./files/" + filename);
+        int nbytes;
+        while((nbytes = input.lee(BUFSIZ)) > 0);
+        istringstream iss(input.get_contenido());
+        vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
+        map<string, int> mapOfWords;
+        for(auto const& token: tokens) {
+            string s = token;
+            // Remove non alpha characters
+            for(int i = 0; i <= s.size(); ++i)
+            {
+                if ((s[i] > ' ' && s[i] <= '@') || s[i]== *u8"¿")
+                    s[i] = ' ';
+            }
+            std::string::iterator end_pos = remove(s.begin(), s.end(), ' ');
+            s.erase(end_pos, s.end());
+            // Not Found
+            if(mapOfWords.find(s) != mapOfWords.end()){          
+                mapOfWords[s]++;
+            }
+            else 
+                mapOfWords[s] = 1;
         }
-        std::string::iterator end_pos = remove(s.begin(), s.end(), ' ');
-        s.erase(end_pos, s.end());
-        // Not Found
-        if(mapOfWords.find(s) != mapOfWords.end()){          
-            mapOfWords[s]++;
+        set<std::pair<string, int>, Comparator> setOfWords(
+                mapOfWords.begin(), mapOfWords.end(), compFunctor);
+        int i=0; 
+        cout << filename << " :" << endl;
+        for (std::pair<std::string, int> element : setOfWords){
+            if (i <= 40){
+                std::cout << "   " <<element.first << " :: " << element.second << std::endl;
+                i++;
+            }
         }
-        else 
-            mapOfWords[s] = 1;
-    }
-    set<std::pair<string, int>, Comparator> setOfWords(
-            mapOfWords.begin(), mapOfWords.end(), compFunctor);
-    int i=0; 
-    cout << filename << " :" << endl;
-    for (std::pair<std::string, int> element : setOfWords){
-        if (i <= 40){
-            std::cout << "   " <<element.first << " :: " << element.second << std::endl;
-            i++;
-        }
-    }
+    } 
     return;
 }
 
@@ -115,6 +123,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
     DIR *directory = opendir("./files");
+    if(directory == NULL)
+    {
+        cout << "Folder '/files' not found" << endl;
+        exit(1);
+    }
     struct dirent *dir;
     while ((dir = readdir(directory)) != NULL)
         if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, ".."))
