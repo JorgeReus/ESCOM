e6de571a040cc29c90d93a10296d60deac75b91c
Jorge Reus <j-g1996@live.com>
2018-04-14 14:52:45 -0500

    Up to date

---
+++ WAD/tarea/Tarea20_LoginStrutsTipoUsuarioLog2_3CM2/src/java/com/myapp/struts/LoginAction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myapp.struts;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ *
+ * @author Reus Gaming PC
+ */
+public class LoginAction extends org.apache.struts.action.Action {
+
+    private final static String SUCCESS = "success";
+    private final static String FAILURE = "failure";
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if((boolean)request.getServletContext().getAttribute("valid")){
+            return mapping.findForward(SUCCESS);
+        } else {
+            return mapping.findForward(FAILURE);
+        }
+    }
+}
