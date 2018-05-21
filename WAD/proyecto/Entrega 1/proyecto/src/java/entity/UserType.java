3f6987846df9618aee9a7506008eef053b4a46e2
BDDLR <bryan.dominguez.delarosa@gmail.com>
2018-05-21 07:08:42 -0500

    Se descomprime el .zip de la entrega #1

---
+++ WAD/proyecto/Entrega 1/proyecto/src/java/entity/UserType.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author reus
+ */
+@Entity
+@Table(name="user_type")
+public class UserType implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="typeId")
+    private Integer typeId;
+    
+    @Column(name="typeName")
+    private String typeName;
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+     
+    
+}
---
+++ WAD/proyecto/Entrega 1/proyecto/src/java/mb/LoginMB.java
@@ -0,0 +1,90 @@
+package mb;
+
+import dao.UserDAO;
+import entity.User;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import util.BussinessConstants;
+import util.NavigationConstants;
+
+/**
+ *
+ * @author Reus Gaming PC
+ */
+@ManagedBean(name = "loginMB")
+@SessionScoped
+public class LoginMB extends GenericMB implements Serializable {
+
+    private UserDAO userDAO;
+    private String user;
+    private String password;
+    private String home;
+
+    public LoginMB() {
+        super();
+    }
+
+    @PostConstruct
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    public String validateLogin() {
+        String redirect = null;
+        User userResult = userDAO.findByUserPassword(user, password);
+        // If its correct, redirect based on the user type
+        if (userResult != null) {
+            getSession().setAttribute("user", userResult.getUser());
+            getSession().setAttribute("userId", userResult.getUserId());
+            getSession().setAttribute("userType", userResult.getUserType().getTypeName());
+            switch (userResult.getUserType().getTypeId()) {
+                case BussinessConstants.USER_TYPE_ADMINISTRATOR:
+                    redirect = NavigationConstants.LOGIN_ADMIN;
+                    break;
+                case BussinessConstants.USER_TYPE_TEACHER:
+                    redirect = NavigationConstants.LOGIN_TEACHER;
+                    break;
+                case BussinessConstants.USER_TYPE_STUDENT:
+                    redirect = NavigationConstants.LOGIN_STUDENT;
+                    break;
+                default:
+                    redirect = NavigationConstants.LOGIN_INDEX;
+                    break;
+            }
+        } else {
+            // If not, show a message
+            addMessage("User/Password Incorrect", "messages", FacesMessage.SEVERITY_ERROR);
+        }
+        home = redirect;
+        return redirect;
+    }
+
+    public String logout() {
+        getSession().invalidate();
+        return NavigationConstants.LOGIN_INDEX;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    public String gotoHome() {
+        return home;
+    }
+
+}
