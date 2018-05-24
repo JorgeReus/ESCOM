150e94fa4662c27a7925f4533cca3aeaf663f60a
Jorge Reus <j-g1996@live.com>
2018-05-24 10:00:42 -0500

    Added image table

---
+++ WAD/proyecto/proyecto/src/java/entity/ImageCategory.java
@@ -0,0 +1,44 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author reus
+ */
+@Entity
+@Table(name = "image_category")
+public class ImageCategory implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "categoryId")
+    private Integer categoryId;
+
+    @Column(name = "categoryName")
+    private String categoryName;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    
+}
