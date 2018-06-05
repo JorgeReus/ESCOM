2888a55d7ecab068e596232d3a7df4d714f2d04b
AndresHGdeG <carlos-andres-hg@hotmail.com>
2018-06-05 09:58:43 -0500

    Se ha cargado el reloj digital al cliente

--- Misc/clock/client.cpp
+++ Misc/clock/client.cpp
@@ -3,7 +3,31 @@
 #include "mensaje.h"
 #include "solicitud.h"
 
+
+#include <iostream>
+#include <string.h>
+#include <cmath>
+#include "gfxModified.h"
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#define SEC_PER_DAY   86400
+#define SEC_PER_HOUR  3600
+#define SEC_PER_MIN   60
+
 #define SERVER_PORT 7200
+
+
+using namespace std;
+
+//Resolución de la pantalla
+#define ANCHURA 800
+#define ALTURA 200
+
+void drawHour(int hour);
+void drawMinute(int min);
+void drawSecond(int sec);
+void drawUSecond(int usec);
 
 int nbdLocal = 0;
 
@@ -12,47 +36,106 @@ int main (int argc, char *argv[]) {
 	Solicitud cliente;
 
 	char *ip;
-	char *args = "100";
+	char *args = "get_Time";
 	int num;
-	if (argc != 3) {
+	if (argc != 2) {
 		printf("Usage: ./client num server_ip\n");
 		return 1;
 	} else {
-		num = atoi(argv[1]);
-		ip = argv[2];
+		
+		ip = argv[1];
 	}
 	char *ms;
 	struct mensaje *msg;
 	int result = 0;
-	for(int i=0; i < num; i++) {
+
+
 		printf("\n\n------Nueva op------\n");
 		ms = cliente.doOperation(ip, SERVER_PORT, 1, args);
-		if(strcmp(ms, "NO") == 0) {
-			printf("Mátate ALV\n");
-			return 1;
-		}
+
+
 		msg = (struct mensaje*)ms;
-		if(atoi(msg->arguments) == nbdLocal) {
-			printf("Read\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
-		} else {
-			printf("Error de lectura\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
-			return 1;
-		}
+		
+		printf("Read\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
+/*
 
 		ms = cliente.doOperation(ip, SERVER_PORT, 2, args);
 		nbdLocal += atoi(args);
-		if(strcmp(ms, "NO") == 0) {
-			printf("Mátate ALV\n");
-			return 1;
-		}
+
 		msg = (struct mensaje*)ms;
-		if(atoi(msg->arguments) == nbdLocal) {
-			printf("Write\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
-		} else {
-			printf("Error de escritura\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
-			return 1;
-		}
+
+		printf("Write\tEl servidor: %d\tLocal: %d\n", atoi(msg->arguments), nbdLocal);
+*/
 		printf("------Fin de op------\n");
-	}
+
+
+int i, contador;
+
+    struct timeval tv;
+    struct timezone tz;
+    int hour, min, sec, usec;
+    long hms;
+
+    gfx_open(ANCHURA, ALTURA, "Ejemplo gfx_display_ascii");
+    gfx_color(0,255,0);
+    
+    while(1) {
+
+        gettimeofday(&tv, &tz);
+
+        hms = tv.tv_sec % SEC_PER_DAY;
+        hms += tz.tz_dsttime * SEC_PER_HOUR;
+        hms -= tz.tz_minuteswest * SEC_PER_MIN;
+        
+        hms = (hms + SEC_PER_DAY) % SEC_PER_DAY;
+          
+        hour = hms / SEC_PER_HOUR;
+        min = (hms % SEC_PER_HOUR) / SEC_PER_MIN;
+        sec = (hms % SEC_PER_HOUR) % SEC_PER_MIN; 
+        usec = tv.tv_usec/100000;
+        
+        gfx_clear();
+
+        drawHour(hour);
+        drawMinute(min);        
+        drawSecond(sec);
+        drawUSecond(usec);
+        
+        usleep(100000);
+    }
+		
+
+
+	
 	return 0;
+}
+
+void drawHour(int hour){
+    int aux = hour%10;
+    hour/=10;
+    
+    gfx_display_ascii(0, 20, 10 , hour+48);
+    gfx_display_ascii(80, 20, 10 , aux+48);
+    gfx_flush();
+}
+
+void drawMinute(int minute){
+    int aux = minute%10;
+    minute/=10;
+    gfx_display_ascii(200, 20, 10 , minute+48);
+    gfx_display_ascii(280, 20, 10 , aux+48);
+    gfx_flush();
+}
+
+void drawSecond(int sec){
+    int aux = sec%10;
+    sec/=10;
+    gfx_display_ascii(400, 20, 10 , sec+48);
+    gfx_display_ascii(480, 20, 10 , aux+48);
+    gfx_flush();
+}
+
+void drawUSecond(int usec){
+    gfx_display_ascii(600, 20, 10 , usec+48);
+    gfx_flush();
 }
