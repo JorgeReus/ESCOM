0832d747ac858744c5fc0f7e9c0ef3719f62a0c7
Jorge Reus <j-g1996@live.com>
2018-06-08 08:29:51 -0500

    Multicast

---
+++ Misc/multicast/client.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <cmath>
+#include <unistd.h>
+
+#define SERVER_PORT 7200
+
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+
+	
+	return 0;
+}
