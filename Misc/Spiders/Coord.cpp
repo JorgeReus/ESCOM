d25dd6dd8d8fb2128186fbcdcdf8f013776f06d0
Jorge Reus <j-g1996@live.com>
2018-06-14 22:30:41 -0500

    Removed misc

--- Misc/Spiders/Coord.cpp
+++
@@ -1,3 +0,0 @@
-#include "Coord.h"
-
-Coord::Coord(int xx, int yy) : x(xx), y(yy) { }
--- Misc/Spiders/Coord.h
+++
@@ -1,9 +0,0 @@
-#ifndef COORD_H_
-#define COORD_H_
-class Coord{
-public:
-    Coord(int = 0, int = 0);
-    int x;
-	int y;
- };
-#endif
